d7611ffb7a0ec483d04e4b1239c45e53c8240f05
Waverider <unstinkable919@gmail.com>
2023-04-20 21:59:15 -0400

    patch

--- Mania3DSQuickBuild.sh
+++ Mania3DSQuickBuild.sh
@@ -2,9 +2,9 @@ git clone https://github.com/Rubberduckycooly/Sonic-Mania-Decompilation
 cd Sonic-Mania-Decompilation
 git checkout 4ef5437
 cd ..
-git clone https://github.com/SaturnSH2x2/RSDKv5-Decompilation --recursive
-cd RSDKv5-Decompilation
-git checkout 3ds-main
+#git clone https://github.com/SaturnSH2x2/RSDKv5-Decompilation --recursive
+#cd RSDKv5-Decompilation
+#git checkout 3ds-main
 rm  Game
 cd ../Sonic-Mania-Decompilation
 mv SonicMania ../RSDKv5-Decompilation/Game
@@ -14,4 +14,5 @@ cd RSDKv5-Decompilation
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=$DEVKITPRO/devkitARM
 make PLATFORM=3DS
-echo "Build done. Binaries are in bin/3DS/CTR."
+echo "Build done. Binaries are in RSDKv5-Decompilation/bin/3DS/CTR."
+echo "To build the CIA, run build_cia.sh in the RSDKv5-Decompilation/3ds directory"
